0154de0bc2b91779af45b20b1091fcf402a1ff5f
rkatarine <rkatarine@mail.ru>
2024-05-19 00:55:53 +0200

    zustand read from state

--- src/zustand/MyRQComponent.tsx
+++ src/zustand/MyRQComponent.tsx
@@ -18,13 +18,23 @@ export const MyRQComponent = ({id}: { id: string }) => {
     const fetcher6 = useItemStore6((state) => state.fetch);
     const fetcher7 = useItemStore7((state) => state.fetch);
     const fetcher8 = useItemStore8((state) => state.fetch);
-    fetcher1();
-    fetcher2();
-    fetcher3();
-    fetcher4();
-    fetcher5();
-    fetcher6();
-    fetcher7();
-    fetcher8();
-    return <div className="Component">{id}</div>;
+
+    const items1 = useItemStore1((state) => state.items);
+    const items2 = useItemStore2((state) => state.items);
+    const items3 = useItemStore3((state) => state.items);
+    const items4 = useItemStore4((state) => state.items);
+    const items5 = useItemStore5((state) => state.items);
+    const items6 = useItemStore6((state) => state.items);
+    const items7 = useItemStore7((state) => state.items);
+    const items8 = useItemStore8((state) => state.items);
+
+    void fetcher1();
+    void fetcher2();
+    void fetcher3();
+    void fetcher4();
+    void fetcher5();
+    void fetcher6();
+    void fetcher7();
+    void fetcher8();
+    return <div className="Component">{id}{items1}{items2}{items3}{items4}{items5}{items6}{items7}{items8}</div>;
 };
